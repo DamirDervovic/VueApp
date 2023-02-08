9ff531177eaadd7851189c6b87759885990cdc17
DamirDervovic <dervovicdamir@gmail.com>
2023-02-08 16:22:11 +0100

    Fetch
    Fetchfunktion uppdaterats, raderat vissa sidor

---
+++ Vue-assignment/src/components/FetchRick.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="flex-container">
+        <button @click="fetchCharacters" class="knappen">
+            Fetch Characters
+        </button>
+        <div
+            v-for="character in characters"
+            :key="character.id"
+            class="main-div"
+        >
+            <div>
+                <h2>{{ character.name }}</h2>
+                <p>Status: {{ character.status }}</p>
+                <p>Species: {{ character.species }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                characters: []
+            }
+        },
+        methods: {
+            async fetchCharacters() {
+                const response = await fetch(
+                    'https://rickandmortyapi.com/api/character?limit=10'
+                )
+                const data = await response.json()
+                this.characters = data.results
+            }
+        }
+    }
+</script>
+
+<style>
+    .flex-container {
+        display: flex;
+        margin-top: 10rem;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .knappen {
+        justify-content: center;
+        margin-bottom: 1rem;
+    }
+    .main-div {
+        height: auto;
+        padding: 0.5rem;
+        width: 10rem;
+        border: 1px solid black;
+        align-self: center;
+        margin: 0.5rem;
+    }
+</style>
--- Vue-assignment/src/main.js
+++ Vue-assignment/src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+import './assets/base.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(bootstrap)
 app.mount('#app')
